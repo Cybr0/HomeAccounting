553f8d3fda978a26d9a3d4280e0bfaa9f9cf2806
Cybr0 <44771809+Cybr0@users.noreply.github.com>
2020-06-20 00:21:48 +0500

    update 0.0.1

---
+++ ConsoleAppTest/ConsoleAppTest/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient
+
+namespace ConsoleAppTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
